76eb20875d1bdbe31b996ebbb62fe2e4f268eaf7
Ilsa <idejager@hotmail.com>
2018-10-16 16:21:24 +0200

    TODO toegevoegd

--- multidim/src/main/java/ilsa/multidim/models/Square.java
+++ multidim/src/main/java/ilsa/multidim/models/Square.java
@@ -24,30 +24,40 @@ public class Square {
 		return rowSize;
 	}
 
-	
+	//TODO afmaken
 	private boolean equalRowSum() {
-		
-		
+		for (int row = 0; row < square.length; row++) {
+
+		}
+
 		return false;
 	}
+
 	
-	
+	//TODO afmaken
 	private boolean equalColumnSum() {
-		
+
+		for (int column = 0; column < square[0].length; column++) {
+			int total = 0;
+			for (int row = 0; row < square.length; row++)
+				total += square[row][column];
+			System.out.println("Sum for column " + column + " is " + total);
+		}
+
 		return false;
 	}
+
 	
+	//TODO afmaken
 	private boolean equalDiagonalSum() {
-		
+
 		return false;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		StringBuilder squareString = new StringBuilder();
-		
+
 		for (int row = 0; row < square.length; row++) {
 			for (int column = 0; column < square[row].length; column++) {
 				squareString.append(square[row][column] + " ");
