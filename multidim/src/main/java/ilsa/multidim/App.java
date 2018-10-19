38756ebfcc8fa96ecd60991c8bceda13aaf1ff99
Ilsa <35494284+iStarRaw@users.noreply.github.com>
2018-10-19 11:31:09 +0200

    correct square bepalen met error handling

--- multidim/src/main/java/ilsa/multidim/App.java
+++ multidim/src/main/java/ilsa/multidim/App.java
@@ -19,8 +19,8 @@ import ilsa.multidim.models.Square;
 public class App {
 	public static void main(String[] args) {
 		final URL FILE_LUNA = App.class.getClassLoader().getResource("Luna.txt");
-		final URL FILE_MERCURY = App.class.getClassLoader().getResource("Mercury.txt");
-		final URL FILE_WEN = App.class.getClassLoader().getResource("wen.txt");
+//		final URL FILE_MERCURY = App.class.getClassLoader().getResource("Mercury.txt");
+//		final URL FILE_WEN = App.class.getClassLoader().getResource("wen.txt");
 
 		List<URL> fileBook = new ArrayList<>();
 
@@ -29,42 +29,45 @@ public class App {
 //		fileBook.add(FILE_WEN);
 
 		for (URL fileName : fileBook) {
-			
-			createSquare(fileName);
 
-//			System.out.println(square.toString());
-//			square.printMagicDetails();
+			try {
+				Square square = createSquare(fileName);
+				System.out.println(square.toString());
+				square.printMagicDetails();
+			} catch (InputMismatchException e) {
+				System.out.println("Input is not a square");
+			}
+
+			
 		}
 
 	}
 
-	private static void createSquare(URL fileName) {
+	private static Square createSquare(URL fileName) throws InputMismatchException {
 		boolean squareAdmitted = isSquare(fileName);
-//		System.out.println(squareAdmitted);
-		
-		
-		
-		if (squareAdmitted) {
-			Square square = new Square(countLines(fileName));
-			
-			try (Scanner readLine = new Scanner(new FileReader(fileName.getFile()))) {
-				while (readLine.hasNextLine()) {
 
-					for (int row = 0; row < square.getSquare().length; row++) {
-						for (int column = 0; column < square.getSquare()[row].length; column++) {
-							square.getSquare()[row][column] = readLine.nextInt();
-						}
+		if (!squareAdmitted) {
+			throw new InputMismatchException();
+		}
+
+		Square square = new Square(countLines(fileName));
+
+		try (Scanner readLine = new Scanner(new FileReader(fileName.getFile()))) {
+			while (readLine.hasNextLine()) {
+
+				for (int row = 0; row < square.getSquare().length; row++) {
+					for (int column = 0; column < square.getSquare()[row].length; column++) {
+						square.getSquare()[row][column] = readLine.nextInt();
 					}
 				}
-
-			} catch (FileNotFoundException e) {
-				System.out.println("File not found!\n");
-			} catch (NoSuchElementException e) {
-				System.out.println("Reached end of file.\n");
 			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!\n");
+		} catch (NoSuchElementException e) {
+			System.out.println("Reached end of file.\n");
 		}
-//		System.out.println("Your input is not a square!");
-
+		return square;
+		
 	}
 
 	private static boolean isSquare(URL fileName) {
